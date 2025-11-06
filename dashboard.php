14efd36954e11bbca266034fa7540144e3ef1fb3
nabil <mhdnabilpratamafajar@gmail.com>
2025-11-06 10:48:04 +0700

    commit 6

--- dashboard.php
+++ dashboard.php
@@ -15,3 +15,20 @@ echo "<p>Selamat datang, " . $_SESSION['username'] . "!</p><hr>";
 $kode_barang = ["B1", "B2", "B3", "B4", "B5"];
 $nama_barang = ["lembu", "kambing", "angsa", "bebek", "ayam"];
 $harga_barang = [25000, 16000, 48000, 37000, 19000];
+
+// Commit 6 – Logika Penjualan Random
+$beli = [];
+$jumlah = [];
+$total = [];
+
+for ($i = 0; $i < count($kode_barang); $i++) {
+    // Random apakah barang dibeli (0 atau 1)
+    $beli[$i] = rand(0, 1);
+    if ($beli[$i] == 1) {
+        $jumlah[$i] = rand(1, 5); // jumlah acak 1–5
+        $total[$i] = $harga_barang[$i] * $jumlah[$i];
+    } else {
+        $jumlah[$i] = 0;
+        $total[$i] = 0;
+    }
+}
